d77e03a86af9f78dd500d0c0031c39466f1702a3
Rajshekhar Prasad Saxena <121630495+cipher2150@users.noreply.github.com>
2026-07-03 12:15:12 +0530

    Spark is continuously polling kafka.

---
+++ Realtime-Customer-Data-Platform/streaming/config.py
@@ -0,0 +1,12 @@
+# Kafka Configuration
+KAFKA_BOOTSTRAP_SERVERS = "localhost:9092" # Kafka broker address
+KAFKA_TOPIC = "customer-events" # Topic spark will read from
+
+# Spark Application
+APP_NAME = "ClickstreamStreaming" # Name shown in Spark UI
+
+# Output Paths
+BRONZE_PATH = "../bronze" # folder where raw events will be stored
+
+# Checkpoint Location
+CHECKPOINT_PATH = "../checkpoints/bronze" #Sores spark progress so it can recover after failure
